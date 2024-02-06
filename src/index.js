cc2490c12d589ae65fc962a92f24aaede587852d
Cade Larrabee <larrabeecade@gmail.com>
2024-02-06 04:45:03 -0500

    initial commit of webpack

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//Photo by <a href="https://unsplash.com/@taylorrfranz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Taylor Franz</a> on <a href="https://unsplash.com/photos/coffee-in-white-ceramic-container-GJogrGZxKJE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+import "./style.css";
+import { projectModule } from "./projectModule.js";
+import { taskModule } from "./taskModule.js";
+
+function OnEntry() {}
+
+// function clearBodyExceptHeaderContainer() {
+//   const bodyChildren = document.body.children;
+
+//   // Iterate through the child nodes of document.body
+//   for (let i = bodyChildren.length - 1; i >= 0; i--) {
+//     const child = bodyChildren[i];
+
+//     // Check if the child is not headercontainer
+//     if (child.className !== "headerContainer") {
+//       // Remove the child node
+//       document.body.removeChild(child);
+//     }
+//   }
+// }
+
+OnEntry();
